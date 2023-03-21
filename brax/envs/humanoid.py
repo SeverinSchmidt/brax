2c1da9518d8b9cf1b6fa1554146b07d642ec0229
SeverinSchmidt <78699853+SeverinSchmidt@users.noreply.github.com>
2023-03-21 13:48:41 +0100

    Updated frozen of holds

--- brax/envs/humanoid.py
+++ brax/envs/humanoid.py
@@ -697,18 +697,18 @@ _SYSTEM_CONFIG = """
   bodies {
     name: "hold1"
     colliders { sphere { radius: 0.05 }
-    position {x: 0.9
+    }
+    frozen { position {x: 0.9
               y: -0.5
-              z: 1.5}}
-    frozen { all: true }
+              z: 1.5}  }
   }
   bodies {
     name: "hold2"
     colliders { sphere { radius: 0.1 }
-    position {x: 0.9
+    }         
+    frozen { position {x: 0.9
               y: 0
-              z: 2.5}}         
-    frozen { position }
+              z: 2.5} }
   }
   joints {
     name: "abdomen_yz"
