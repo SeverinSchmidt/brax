8d7b4bafc85a10d4f86226a94d7a3ef338bc02a9
SeverinSchmidt <78699853+SeverinSchmidt@users.noreply.github.com>
2023-05-08 13:52:34 +0200

    Changed scale to 1

--- brax/envs/humanoid_lidar.py
+++ brax/envs/humanoid_lidar.py
@@ -282,18 +282,18 @@ class Humanoid(env.Env):
 #     target_dir = target_rel / (1e-6 + target_dist)
 #     moving_to_target = .5 * jp.dot(torso_delta, target_dir)
 
-#     torso_delta = com_after - com_before
-#     target_rel = jp.array([0.5, 0, 3.7]) - com_after
-#     target_dist = jp.norm(target_rel)
-#     target_dir = target_rel / (1e-6 + target_dist)
-#     moving_to_target = 10 * jp.dot(torso_delta, target_dir)
-
-    target = jp.array([0.5, 0, 3.7])
-    target_rel = target-com_after
+    torso_delta = com_after - com_before
+    target_rel = jp.array([0.5, 0, 3.7]) - com_after
     target_dist = jp.norm(target_rel)
-    start_dist = jp.norm(target)
-    distance_moved = start_dist - target_dist
-    moving_to_target = distance_moved**2
+    target_dir = target_rel / (1e-6 + target_dist)
+    moving_to_target = 1000 * jp.dot(torso_delta, target_dir)
+
+#     target = jp.array([0.5, 0, 3.7])
+#     target_rel = target-com_after
+#     target_dist = jp.norm(target_rel)
+#     start_dist = jp.norm(target)
+#     distance_moved = start_dist - target_dist
+#     moving_to_target = distance_moved**2
 
     
    
@@ -720,9 +720,8 @@ _SYSTEM_CONFIG = """
   
   bodies {
     name: "wall" mass: 1000
-    colliders { mesh {name: "lidar_stair" scale: 0.36}
+    colliders { mesh {name: "lidar_stair" scale: 1}
     position { x: 0.22 y: 0 z: 0}
-    material { friction: 1}
     }
     
     colliders { sphere { radius: 0.1 }
