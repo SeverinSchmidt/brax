58af39626e9601792971441058351e32acba8d01
SeverinSchmidt <78699853+SeverinSchmidt@users.noreply.github.com>
2023-05-14 16:08:12 +0200

    Cleaning up humanoid_path.py

--- brax/envs/humanoid_path.py
+++ brax/envs/humanoid_path.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Trains a humanoid to run in the +x direction."""
+"""Trains a humanoid to climb up a Blender-modelled steep staircase with added obstacles."""
 
 import brax
 from brax import jumpy as jp
